beee7490c5abe75bdbf5e36a6a578674cffb558f
mkdir-helen <experiment.helen@gmail.com>
2019-01-21 20:18:30 -0500

    setting up the basics

---
+++ src/features/contact-list/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import fetch from 'isomorphic-fetch';
+import Contact from './contact';
+
+class ContactList extends React.Component {
+    state = {
+        contacts: []
+    }
+    componentDidMount() {
+        this.loadContacts();
+    }
+    loadContacts = () => {
+        fetch('https://student-example-api.herokuapp.com/v1/contacts.json')
+            .then(response => response.json())
+            .then(json => this.setState({
+                contacts: json.contacts
+            }))
+    }
+
+    render() {
+        return <ul className="contacts">
+            {
+                this.state.contacts.map(contact => <li>
+                    <Contact {...contact} />
+                </li>)
+            }
+        </ul>
+    }
+}
+
+export default ContactList;
